2743416ac2933f5ae62243cfb594b7153154d686
Maksim Zayakin <rasetsuq@yandex.ru>
2024-12-12 11:49:05 +0500

    feat: add FindnLedEntity

--- custom_components/findn_led_ble/entity.py
+++ custom_components/findn_led_ble/entity.py
@@ -1,20 +1,17 @@
-"""BlueprintEntity class."""
+"""FindnLedEntity class."""
 
 from __future__ import annotations
 
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import ATTRIBUTION
-from .coordinator import BlueprintDataUpdateCoordinator
+from .coordinator import FindnLedDataUpdateCoordinator
 
 
-class IntegrationBlueprintEntity(CoordinatorEntity[BlueprintDataUpdateCoordinator]):
+class FindnLedEntity(CoordinatorEntity[FindnLedDataUpdateCoordinator]):
     """BlueprintEntity class."""
 
-    _attr_attribution = ATTRIBUTION
-
-    def __init__(self, coordinator: BlueprintDataUpdateCoordinator) -> None:
+    def __init__(self, coordinator: FindnLedDataUpdateCoordinator) -> None:
         """Initialize."""
         super().__init__(coordinator)
         self._attr_unique_id = coordinator.config_entry.entry_id
